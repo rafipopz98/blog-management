3916a7f80467b36246c6e921aae9d46950bb02e2
rafipopz98 <itsrealrafi@gmail.com>
2025-08-25 03:08:51 +0530

    rules off

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -20,6 +20,19 @@ const eslintConfig = [
       "next-env.d.ts",
     ],
   },
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    languageOptions: {
+      parserOptions: {
+        project: "./tsconfig.json",
+      },
+    },
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "react/no-unescaped-entities": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
